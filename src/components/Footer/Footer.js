df92960d4a5910a623cfe1cf7151983289dda6e6
danielAkpudi-2021 <akpudidaniel109@gmail.com>
2022-10-22 07:50:49 -0700

    remove whatapp icon

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { AiOutlineTwitter, AiFillGithub, AiOutlineInstagram, AiOutlineFacebook, AiOutlineWhatsApp
+import { AiOutlineTwitter, AiFillGithub, AiOutlineInstagram, AiOutlineFacebook
 } from 'react-icons/ai';
 
 import './Footer.scss'
@@ -16,7 +16,6 @@ const Footer = () => {
            <a href='https://github.com/danielAkpudi-2021' target='_blank'><span><AiFillGithub /></span></a>
             <a href=''><span><AiOutlineInstagram /></span></a>
             <a href=''><span><AiOutlineFacebook /></span></a>
-            <a href=''><span><AiOutlineWhatsApp /></span></a>
         </div>
     </div>
   )
